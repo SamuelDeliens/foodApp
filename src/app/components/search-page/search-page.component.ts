0b2ddf53e8675c55b8f5597f28a5b69309ec11dd
jiji20100 <jihadrifi909@gmail.com>
2023-11-02 00:02:47 +0100

    ajout d'Api

--- src/app/components/search-page/search-page.component.ts
+++ src/app/components/search-page/search-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ImageService } from '../../services/image.service';
 
 @Component({
   selector: 'app-search-page',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-page.component.css']
 })
 export class SearchPageComponent {
+  images: any[] = [];
+  query: string = '';
+  constructor(private imageService: ImageService) {}
+
+  search(): void {
+    this.imageService.searchImages(this.query)
+      .subscribe(data => {
+        console.log(data); // Affichez la réponse de l'API dans la console
+        this.images = data.hits;
+      });
+  }
+  
+
 
 }
