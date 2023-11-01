2243ecc565744b1c1dc87ddbf5d12e5f246a39b7
SamuelDeliens <samuel.deliens@gmail.com>
2023-11-01 19:28:24 +0100

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomePageComponent} from "./components/home-page/home-page.component";
 import {SearchPageComponent} from "./components/search-page/search-page.component";
 import {DiscoverPageComponent} from "./components/discover-page/discover-page.component";
+import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomePageComponent },
   { path: 'search', component: SearchPageComponent },
   { path: 'discover', component: DiscoverPageComponent },
+  { path: '**', component: NotFoundPageComponent },
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { MainNavComponent } from "./components/main-nav/main-nav.component";
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'searchApp';
+  title = 'foodApp';
 }
