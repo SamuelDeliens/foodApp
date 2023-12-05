13a7135d366531dbc717b25c72e8818f683dbd7f
halice <115983607+halice6C@users.noreply.github.com>
2023-12-05 01:37:36 +0100

    deleted unused variable safeURL

--- src/app/components/page-components/recipe-details-page/recipe-details-page.component.ts
+++ src/app/components/page-components/recipe-details-page/recipe-details-page.component.ts
@@ -13,8 +13,7 @@ import {ImageSearchService} from "../../../services/image-search.service";
 export class RecipeDetailsPageComponent {
 
   private idRecipe: string = "";
-  public fetchCocktailDetails!:any;
-  public safeURL!: SafeResourceUrl;
+  public fetchCocktailDetails!:any; 
   public videoURL!: string; 
   @Input()
   recipe: RecipeDetails = new RecipeDetails("");
