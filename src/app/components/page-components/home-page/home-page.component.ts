eaa052fe4c2daec48e938120cdc20a09340e4c9b
halice <115983607+halice6C@users.noreply.github.com>
2023-12-05 22:22:20 +0100

    added food feed and cocktail one

--- src/app/components/page-components/home-page/home-page.component.ts
+++ src/app/components/page-components/home-page/home-page.component.ts
@@ -4,6 +4,8 @@ import { SearchBarComponent } from "../../search-bar/search-bar.component";
 import {Router} from "@angular/router";
 import { FeedService } from 'src/app/services/feed.service';
 import { CocktailService } from 'src/app/services/cocktail.service';
+import { FoodService } from 'src/app/services/food.service';
+import { Recipe } from 'src/app/data/recipe';
 
 @Component({
   selector: 'app-home-page',
@@ -13,16 +15,27 @@ import { CocktailService } from 'src/app/services/cocktail.service';
 })
 export class HomePageComponent {
 
-  constructor(private appComponent: AppComponent, private router: Router,private feedService:FeedService, private cocktailService:CocktailService) {
+  constructor(private appComponent: AppComponent, private router: Router,private foodService:FoodService, private cocktailService:CocktailService) {
   }
-  public searchFeed!:any;
+  public searchFirstRowFoodFeed!:any;
+  public searchSecondRowFoodFeed!:any;
+  public searchThirdRowFoodFeed!:any;
+  public searchFourthRowFoodFeed!:any;
   public searchCocktailFeed!:any;
   ngOnInit(): void {
 
     this.cocktailService.searchPopularCocktails().subscribe((result) =>{
+  
       this.searchCocktailFeed = result
       console.log('Données récupérées :', result);
     });
+    this.foodService.searchRecipes("").subscribe((data)=>{
+      this.searchFirstRowFoodFeed = data.results.slice(0, 2).map((recipe:any)=> new Recipe("food", recipe))
+      this.searchSecondRowFoodFeed = data.results.slice(2, 4).map((recipe:any)=> new Recipe("food", recipe))
+      this.searchThirdRowFoodFeed = data.results.slice(4, 6).map((recipe:any)=> new Recipe("food", recipe))
+      this.searchFourthRowFoodFeed = data.results.slice(6, 8).map((recipe:any)=> new Recipe("food", recipe))
+
+    })
   }
 
   get appTitle() {
@@ -36,7 +49,7 @@ export class HomePageComponent {
     this.router.navigate(['food', query]);
   }
   onCocktailClick(id: string) {
-    // Logique personnalisée
+
     this.router.navigate(['/recipe/food', id]);
   }
 }
